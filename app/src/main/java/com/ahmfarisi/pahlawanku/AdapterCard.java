6232a924ce653b2961e5a60f11afe463b38bb25d
ahmfarisi <ahmfarisi@gmail.com>
2022-11-16 14:13:15 +0700

    Update AdapterCard

--- app/src/main/java/com/ahmfarisi/pahlawanku/AdapterCard.java
+++ app/src/main/java/com/ahmfarisi/pahlawanku/AdapterCard.java
@@ -1,13 +1,37 @@
 package com.ahmfarisi.pahlawanku;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterCard {
+import java.util.ArrayList;
+
+public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder> {
+    private ArrayList<ModelPahlawan> dataPahlawan;
+
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+        this.dataPahlawan = dataPahlawan;
+    }
+
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ClassViewHolder extends RecyclerView.ViewHolder {
         ImageView ivFoto;
