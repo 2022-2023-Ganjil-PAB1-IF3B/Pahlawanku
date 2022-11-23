2fbd7b5dcde58d097d22895501b8387953c905ca
ahmfarisi <ahmfarisi@gmail.com>
2022-11-23 14:44:45 +0700

    Update MainActivity.java

--- app/src/main/java/com/ahmfarisi/pahlawanku/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/pahlawanku/MainActivity.java
@@ -1,11 +1,14 @@
 package com.ahmfarisi.pahlawanku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -35,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
         rvPahlawan.setLayoutManager(new GridLayoutManager(this, 2));
         AdapterGrid varAdapterGrid = new AdapterGrid(data, MainActivity.this);
         rvPahlawan.setAdapter(varAdapterGrid);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mode, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_mode_card:
+                tampilDataCard();
+                break;
+            case R.id.menu_mode_grid:
+                tampilDataGrid();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
